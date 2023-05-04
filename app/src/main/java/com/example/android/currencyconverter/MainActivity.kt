09e835d72a6ad51f287cdd5830a06dfae95daa9a
Intesar <intesar.alawthali@talabat.com>
2023-05-04 15:08:46 +0400

    First Commit

---
+++ app/src/main/java/com/example/android/currencyconverter/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.android.currencyconverter
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        //hfjh
+        setContentView(R.layout.activity_main)
+    }
+}
